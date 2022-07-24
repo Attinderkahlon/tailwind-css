8fe25c19c035d707e563382cc8bfbd074df7c8e2
Attinderpal singh <attinderkahlon28@gmail.com>
2022-07-24 12:27:21 +1000

    added images and logo folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <div className="text-3xl font-bold underline">Tailwind CSS</div>
+  return (
+    <>
+      <nav className="relative container mx-auto p-6">
+        <div className="flex items-center justify-between">
+          {/* logo */}
+          <div className="pt-2">
+            <img src={"img/logo.svg"} alt="" />
+          </div>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default App
